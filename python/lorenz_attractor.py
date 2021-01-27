0f138447581d80b6ac81982b8bcf4feacbb956dd
Ayden <cookayden2003@gmail.com>
2021-01-26 23:20:22 -0500

    lorenz attractor

---
+++ python/lorenz_attractor.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from mpl_toolkits.mplot3d import Axes3D
+
+rho = 28.0
+sigma = 10.0
+beta = 8.0 / 3.0
+
+def f(state, t):
+    x, y, z = state  # Unpack the state vector
+    return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  # Derivatives
+
+state0 = [1.0, 1.0, 1.0]
+t = np.arange(0.0, 40.0, 0.01)
+
+states = odeint(f, state0, t)
+
+fig = plt.figure()
+ax = fig.gca(projection="3d")
+ax.plot(states[:, 0], states[:, 1], states[:, 2])
+plt.draw()
+plt.show()
