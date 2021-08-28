b5137b85b8ade55330e90f80f581e7b3686ade4d
Ayden <cookayden2003@gmail.com>
2021-08-28 16:32:46 -0400

    wave equation update

--- python/pygame/2d_wave_equation.py
+++ python/pygame/2d_wave_equation.py
@@ -13,10 +13,10 @@ fps = 120
 
 
 class Point:
-    def __init__(self, index, y):
+    def __init__(self, index, y, yV):
         self.index = index
         self.y = y
-        self.yV = 0
+        self.yV = yV
         self.yA = 0
 
     def simulate(self):
@@ -25,17 +25,25 @@ class Point:
 
 
 # little pieces of string
+points = []
 pointCount = 400
 pointWidth = width/pointCount
 stringConstant = 30000
+dampening = 0.5
 
-# initial heights of pieces
-# sine wave
-#points = [Point(i, height/4 * math.sin(i*pointWidth * 2*math.pi/width)) for i in range(pointCount)] # y coordinate of each piece
-# bell curve
-#points = [Point(i, height/6 * math.e**(-((i*pointWidth - width/2)/width/0.1)**2)) for i in range(pointCount)]
+def reset():
+    global points
+    # initial heights of pieces
+    # sine wave
+    #points = [Point(i, height/4 * math.sin(i*pointWidth * 2*math.pi/width)) for i in range(pointCount)] # y coordinate of each piece
+    # bell curve
+    #points = [Point(i, height/6 * math.e**(-((i*pointWidth - width/2)/width/0.1)**2)) for i in range(pointCount)]
 
-points = [Point(i, (height/2 * math.e**(-((i*pointWidth - width/2)/(width*0.1))**2)) + (-height/5 * math.e**(-((pointWidth*i - width/2)/(0.2*width))**2)) ) for i in range(pointCount)]
+    # mix
+    points = [Point(i, height/4 * math.sin(i*pointWidth * 2*math.pi/width) + height/8 * math.sin(i*pointWidth * 6*math.pi/width) + height/2 * math.e**(-((i*pointWidth - width/2)/width/0.1)**2), 0) for i in range(pointCount)]
+
+    #points = [Point(i, (height/2 * math.e**(-((i*pointWidth - width/2)/(width*0.1))**2)) + (-height/5 * math.e**(-((pointWidth*i - width/2)/(0.2*width))**2)), 0) for i in range(pointCount)]
+
 
 def drawString():
     for i in range(pointCount-1):
@@ -52,10 +60,13 @@ def simulateString():
         y = points[i].y
         rightY = points[i+1].y
 
-        points[i].yA = (stringConstant/(pointWidth**2)) * (rightY - 2*y + leftY)
+        points[i].yA = (stringConstant/(pointWidth**2)) * (rightY - 2*y + leftY) - dampening*points[i].yV
 
     for point in points:
         point.simulate()
+
+
+reset()
 
 while running:
     clock.tick_busy_loop(fps)
@@ -67,6 +78,8 @@ while running:
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_q:
                 running = False
+            if event.key == pg.K_r:
+                reset()
 
     screen.fill((70, 70, 70))
 
