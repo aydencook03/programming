1213c2e0df85a3d62b5afe19bef1d32f7ec26521
Ayden <cookayden2003@gmail.com>
2022-02-18 14:01:33 -0500

    2d wave update

--- python/pygame/2d-waves.py
+++ python/pygame/2d-waves.py
@@ -3,22 +3,26 @@
 # SETUP
 
 import pygame as pg
+import math
 
 
 running = False
 simulating = False
 recording = False
 FPS = 24
-simFPS = 120
+simFPS = 30
 clock = pg.time.Clock()
 
 res = (256, 256)
 
 bgColor = (72, 72, 72)
 crimson = (220, 20, 60)
-green = (119, 221, 119)
+forestGreen = (0,110,51)
+teal = (0,124,128)
+
+peakColor = crimson
+crestColor = teal
 peakHeight = 0
-crestHeight = 0
 
 pg.init()
 screen = pg.display.set_mode(res)
@@ -26,12 +30,12 @@ pg.display.set_caption("Simulation")
 
 pixels = pg.PixelArray(screen)
 
-mesh = [[(x,y) for y in range(res[1])] for x in range(res[0])]
+mesh = [[[0, 0, 0] for y in range(res[1])] for x in range(res[0])] # mesh[x][y][height, vel, accel]
 
-waveVelocity = (res[0]/3)/simFPS
+waveVelocity = res[0]/3
 
 ######################################################################################
-# LOGIC
+# INITIALIZE
 
 def restart():
     global running
@@ -42,12 +46,38 @@ def restart():
     loop()
 
 def resetMesh():
+    global peakHeight
+    peakHeight = -math.inf
+
     xRange = (-4, 4)
     yRange = (-4, 4)
+    shape = lambda x,y: math.exp(-(x**2 + y**2))
+
+    for i in range(res[0]):
+        for j in range(res[1]):
+            x = xRange[0] + i*(xRange[1] - xRange[0])/res[0]
+            y = yRange[0] + j*(yRange[1] - yRange[0])/res[1]
+
+            mesh[i][j] = shape(x, y)
+            peakHeight = mesh[i][j] if mesh[i][j] > peakHeight else peakHeight
+
+######################################################################################
+# GRAPHICS
+
+def interpolateColor(height):
+    return (255, 255, 255)
 
 def paintScreen():
-    pixels[:][:] = crimson
+    for x in range(res[0]):
+        for y in range(res[1]):
+            colors = interpolateColor(mesh[x][y])
+            r = colors[0]
+            g = colors[1]
+            b = colors[2]
+            pixels[x][-1 - y] = (r, g, b)
 
+######################################################################################
+# MATH
 
 def firstDiff(mesh, x, y, dimension):
     value = 0
@@ -94,5 +124,6 @@ def loop():
         clock.tick_busy_loop(simFPS)
 
 ######################################################################################
+# START LOOP
 
 restart()
