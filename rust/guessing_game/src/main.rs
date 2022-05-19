3959b318498ee3f81ce4bc1d31e2b36c1b2689cc
Ayden Cook <cookayden2003@gmail.com>
2022-05-18 21:48:21 -0400

    finished guessing game

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,20 +1,32 @@
-use std::io;
 use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number: u8 = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is {}", secret_number);
+    loop {
+        println!("Input your guess, from 1 to 100:");
 
-    println!("Input your guess:");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        let _input_size = io::stdin().read_line(&mut guess).expect("Failed to read line.");
 
-    let input = io::stdin();
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
-    let size = input.read_line(&mut guess).expect("Failed to read line");
-
-    println!("You guessed {}, this input is {} bytes", guess, size);
+        if guess == secret_number {
+            println!("You got it!");
+            break;
+        } else {
+            if guess < secret_number {
+                println!("Too low!");
+            } else {
+                println!("Too high!");
+            }
+        }
+    }
 }
